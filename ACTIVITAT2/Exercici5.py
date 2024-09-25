d60debd5f85f9a44088bc49dd61ebef73075a6ed
Achraf Chakir <2023_achraf.chakir@iticbcn.cat>
2024-09-25 20:34:34 +0200

    Exercici 5

---
+++ ACTIVITAT2/Exercici5.py
@@ -0,0 +1,7 @@
+numero = int(input("Introdueix un número: "))
+
+# Comprovar si el numero és parell o senar
+if numero % 2 == 0:
+    print("El número és parell.")
+else:
+    print("El número és senar.")
