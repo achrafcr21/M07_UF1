f3b5ad1db3c6546c45c45fb0fe1ee1c4b5d631f6
Achraf Chakir <2023_achraf.chakir@iticbcn.cat>
2024-09-25 20:10:57 +0200

    Exercici 3

---
+++ ACTIVITAT2/Exercici3.py
@@ -0,0 +1,9 @@
+valor_1 = int(input("Introduiex el primer valor: "))
+valor_2 = int(input("Introduiex el segon valor: "))
+
+if valor_1 > valor_2 : 
+    print("El valor més gran és: ", valor_1)
+elif valor_1 < valor_2:
+    print("El valor més gran és: ", valor_2)
+else : 
+    print("Els valors són iguals")
